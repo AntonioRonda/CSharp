b5c535be51b18487930dce689ca3769db83737c2
Green\AntonioRonda <antonio.ronda@sophos.com>
2017-07-09 18:54:03 +0800

    07/09/2017 - Selenium Final Assesment v 0.2

--- SeleniumTesting/SeleniumTesting/Program.cs
+++ SeleniumTesting/SeleniumTesting/Program.cs
@@ -9,23 +9,18 @@ namespace SeleniumTesting
     class Program
     {
 
-        //Create Reference for the browser
-
-
         private readonly IWebDriver _driver = new ChromeDriver();
 
-        
+
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Running Final Assesment Selenium Testing in C#.....");
         }
 
         [OneTimeSetUp]
 
         public void Initialize()
         {
-
-            //Navigate to SignUp
             _driver.Navigate().GoToUrl("https://www.linkedin.com/start/join?trk=hb_join");
             Console.WriteLine("Opening the Browser.....");
         }
@@ -36,27 +31,33 @@ namespace SeleniumTesting
 
         public void SignUpThruEmail()
         {
-            Console.WriteLine("Executing Test SignUp.....");
+            Console.WriteLine("Executing Test SignUp Function.....");
             IWebElement firstNameElement = _driver.FindElement(By.Name("firstName"));
             firstNameElement.SendKeys("Tony");
             IWebElement lastNameElement = _driver.FindElement(By.Name("lastName"));
             lastNameElement.SendKeys("Parker");
             IWebElement emailAddressElement = _driver.FindElement(By.Name("emailAddress"));
-            emailAddressElement.SendKeys("ar989g1@gmail.com");
+            emailAddressElement.SendKeys("ar989g2@gmail.com");
             IWebElement passwordElement = _driver.FindElement(By.Name("password"));
             passwordElement.SendKeys("Aenipainahch@9");
             IWebElement joinButtonElement = _driver.FindElement(By.XPath("//*[@id=\"uno-reg-join\"]/div/div/div/div[2]/div[1]/div/div/form/fieldset/button/span"));
             joinButtonElement.Submit();
-            IWebElement countryCodeElement = _driver.FindElement(By.Name("countryCode"));
-            var selectElement = new SelectElement(countryCodeElement);
-            selectElement.SelectByIndex(167);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            IWebElement countryCodeElement = _driver.FindElement(By.Id("country-select"));
+            var selectCountryElement = new SelectElement(countryCodeElement);
+            selectCountryElement.SelectByValue("ph");
+            IWebElement numberInputElement = _driver.FindElement(By.Name("number-input"));
+            numberInputElement.SendKeys("9985928605");
+            IWebElement sendCodeElement = _driver.FindElement(By.XPath("//*[@id=\"submitChallenge\"]"));
+            sendCodeElement.Submit();
+
         }
 
         [Test]
 
         public void SignIn()
         {
-            Console.WriteLine("Executing Test SignIn.....");
+            Console.WriteLine("Executing Test SignIn Function.....");
             _driver.Navigate().GoToUrl("https://www.linkedin.com/uas/login");
             Console.WriteLine("Opening the Browser.....");
             IWebElement sessionKeyElement = _driver.FindElement(By.Name("session_key"));
@@ -75,7 +76,7 @@ namespace SeleniumTesting
         public void WriteAnArticle()
         {
             SignIn();
-            Console.WriteLine("Executing Test WriteAnArticle.....");
+            Console.WriteLine("Executing Test WriteAnArticle Function.....");
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             IWebElement sharePostElement = _driver.FindElement(By.ClassName("sharing-share-box__post-button"));
             sharePostElement.SendKeys(Keys.Enter);
@@ -89,10 +90,10 @@ namespace SeleniumTesting
         [Test]
 
 
-        public void SearchConnection()
+        public void SearchPeople()
         {
             SignIn();
-            Console.WriteLine("Executing Test SearchConnection.....");
+            Console.WriteLine("Executing Test SearchPeople Function.....");
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             IWebElement searchFieldElement = _driver.FindElement(By.ClassName("ember-text-field"));
             searchFieldElement.SendKeys("Antonio Santos Ronda Jr.");
