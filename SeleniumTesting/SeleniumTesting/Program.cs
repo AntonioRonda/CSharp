a134fcc0b4641c751210fe980dc01b88a77c22cb
Green\AntonioRonda <antonio.ronda@sophos.com>
2017-07-09 16:37:43 +0800

    Selenium Testing

--- SeleniumTesting/SeleniumTesting/Program.cs
+++ SeleniumTesting/SeleniumTesting/Program.cs
@@ -9,6 +9,7 @@ using NUnit.Core;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
+using OpenQA.Selenium.Support.PageObjects;
 
 namespace SeleniumTesting
 {
@@ -16,92 +17,99 @@ namespace SeleniumTesting
     {
 
         //Create Reference for the browser
-        IWebDriver driver = new ChromeDriver();
-
+        private readonly IWebDriver _driver = new ChromeDriver();
 
         static void Main(string[] args)
         {
 
         }
 
-        [SetUp]
+        [OneTimeSetUp]
 
         public void Initialize()
         {
 
-            ////Navigate to SignUp
-            //driver.Navigate().GoToUrl("https://www.linkedin.com/start/join?trk=hb_join");
-            //Console.WriteLine("Opening the Browser.....");
-            //Navigate to SignIn
-            driver.Navigate().GoToUrl("https://www.linkedin.com/uas/login");
+            //Navigate to SignUp
+            _driver.Navigate().GoToUrl("https://www.linkedin.com/start/join?trk=hb_join");
             Console.WriteLine("Opening the Browser.....");
-            //Post an Article
-            //driver.Navigate().GoToUrl("https://www.linkedin.com/feed/");
-            //Console.WriteLine("Opening the Browser.....");
-
-
         }
 
 
-        //[Test]
 
-        //public void SignUp()
-        //{
-        //    Console.WriteLine("Executing Test.....");
-        //    IWebElement firstNameElement = driver.FindElement(By.Name("firstName"));
-        //    firstNameElement.SendKeys("Tony");
-        //    IWebElement lastNameElement = driver.FindElement(By.Name("lastName"));
-        //    lastNameElement.SendKeys("Parker");
-        //    IWebElement emailAddressElement = driver.FindElement(By.Name("emailAddress"));
-        //    emailAddressElement.SendKeys("ar989g@gmail.com");
-        //    IWebElement passwordElement = driver.FindElement(By.Name("password"));
-        //    passwordElement.SendKeys("Aenipainahch@9");
-        //    IWebElement joinButtonElement = driver.FindElement(By.XPath("//*[@id=\"uno-reg-join\"]/div/div/div/div[2]/div[1]/div/div/form/fieldset/button/span"));
-        //    joinButtonElement.Submit();
+        [Test]
 
-        //}
+        public void SignUp()
+        {
+            Console.WriteLine("Executing Test SignUp.....");
+            IWebElement firstNameElement = _driver.FindElement(By.Name("firstName"));
+            firstNameElement.SendKeys("Tony");
+            IWebElement lastNameElement = _driver.FindElement(By.Name("lastName"));
+            lastNameElement.SendKeys("Parker");
+            IWebElement emailAddressElement = _driver.FindElement(By.Name("emailAddress"));
+            emailAddressElement.SendKeys("ar989g@gmail.com");
+            IWebElement passwordElement = _driver.FindElement(By.Name("password"));
+            passwordElement.SendKeys("Aenipainahch@9");
+            IWebElement joinButtonElement = _driver.FindElement(By.XPath("//*[@id=\"uno-reg-join\"]/div/div/div/div[2]/div[1]/div/div/form/fieldset/button/span"));
+            joinButtonElement.Submit();
 
-        //[Test]
+        }
 
-        //public void SignIn()
-        //{
+        [Test]
 
-        //    Console.WriteLine("Executing Test.....");
-        //    IWebElement sessionKeyElement = driver.FindElement(By.Name("session_key"));
-        //    sessionKeyElement.SendKeys("ar989g@gmail.com");
-        //    IWebElement sessionPasswordElement = driver.FindElement(By.Name("session_password"));
-        //    sessionPasswordElement.SendKeys("Aenipainahch@9");
-        //    IWebElement signInButtonElement = driver.FindElement(By.Name("signin"));
-        //    signInButtonElement.Submit();
-
-        //}
+        public void SignIn()
+        {
+            Console.WriteLine("Executing Test SignIn.....");
+            _driver.Navigate().GoToUrl("https://www.linkedin.com/uas/login");
+            Console.WriteLine("Opening the Browser.....");
+            IWebElement sessionKeyElement = _driver.FindElement(By.Name("session_key"));
+            sessionKeyElement.SendKeys("ar989g@gmail.com");
+            IWebElement sessionPasswordElement = _driver.FindElement(By.Name("session_password"));
+            sessionPasswordElement.SendKeys("Aenipainahch@9");
+            IWebElement signInButtonElement = _driver.FindElement(By.Name("signin"));
+            signInButtonElement.Submit();
+        }
 
         [Test]
 
         public void WriteAnArticle()
         {
-            //SignIn
-            Console.WriteLine("Executing Test.....");
-            IWebElement sessionKeyElement = driver.FindElement(By.Name("session_key"));
-            sessionKeyElement.SendKeys("ar989g@gmail.com");
-            IWebElement sessionPasswordElement = driver.FindElement(By.Name("session_password"));
-            sessionPasswordElement.SendKeys("Aenipainahch@9");
-            IWebElement signInButtonElement = driver.FindElement(By.Name("signin"));
-            signInButtonElement.Submit();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
-            //Double Click the Share Field
-            IWebElement postButtonElement = driver.FindElement(By.XPath("//*[@id=\"ember3471\"]")));
-            postButtonElement.Click();
+            SignIn();
+            Console.WriteLine("Executing Test WriteAnArticle.....");
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            IWebElement sharePostElement = _driver.FindElement(By.ClassName("sharing-share-box__post-button"));
+            sharePostElement.SendKeys(Keys.Enter);
+            IWebElement writePostElement = _driver.FindElement(By.ClassName("sharing-textarea__textarea--original"));
+            writePostElement.SendKeys("Hello");
+            IWebElement PostElement = _driver.FindElement(By.ClassName("post"));
+            PostElement.SendKeys(Keys.Enter);
+        }
+
+
+        [Test]
+
+
+        public void SearchConnection()
+        {
+            SignIn();
+            Console.WriteLine("Executing Test SearchConnection.....");
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            IWebElement searchFieldElement = _driver.FindElement(By.ClassName("ember-text-field"));
+            searchFieldElement.SendKeys("Antonio Santos Ronda Jr.");
+            IWebElement navigateSearchButtonElement = _driver.FindElement(By.ClassName("nav-search-button"));
+            navigateSearchButtonElement.SendKeys(Keys.Enter);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            IWebElement actorNameElement = _driver.FindElement(By.PartialLinkText("Antonio Santos Ronda Jr."));
+            actorNameElement.Click();
+
         }
 
         [TearDown]
         public void Cleanup()
         {
-            Thread.Sleep(5000);
-            driver.Close();
+            _driver.Close();
             Console.WriteLine("Closing the Browser.....");
-
         }
 
     }
+
 }
